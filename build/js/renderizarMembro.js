201b61a9ecc132bc8264f45745225662b3d9e85f
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-03-14 14:12:14 -0300

    Renderiza box principal

---
+++ build/js/renderizarMembro.js
@@ -0,0 +1,60 @@
+const renderizarMembro = ({id, foto, nome, cargo, idade}, posicao) => {
+    const gridMembros = document.querySelector('.grid__membros');
+    const jsonData  = {'id':id, 'foto':foto, 'nome':nome, 'cargo':cargo, 'idade':idade};
+
+    const membro = document.createElement('div')
+          membro.setAttribute('class', 'grid-member')
+
+    const boxFoto = document.createElement('div')
+    const boxDados = document.createElement('div')
+
+    const checkbox = document.createElement('input')
+          checkbox.setAttribute('name', 'radio')
+          checkbox.setAttribute('type', 'radio') 
+          checkbox.setAttribute('class', 'checkded__member')
+          checkbox.setAttribute('data-json', JSON.stringify(jsonData))  
+    
+    const idMembro    = document.createElement('span');
+          idMembro.textContent = id;  
+    
+    const fotoMembro  = document.createElement('img');
+          fotoMembro.setAttribute('src', `/assets/img/${foto}`);
+    
+    const nomeMembro  = document.createElement('span');
+          nomeMembro.textContent = nome;
+
+    const cargoMembro = document.createElement('span');
+          cargoMembro.textContent = cargo;
+
+    const idadeMembro = document.createElement('span');
+          idadeMembro.textContent = idade;
+
+
+    boxFoto.appendChild(idMembro)
+    boxFoto.appendChild(fotoMembro)
+
+    boxDados.appendChild(nomeMembro)
+    boxDados.appendChild(cargoMembro)
+    boxDados.appendChild(idadeMembro)
+
+    membro.appendChild(boxFoto)
+    membro.appendChild(boxDados)
+    membro.appendChild(checkbox);
+
+    
+      if (posicao == 'prepend') {
+            gridMembros.prepend(membro);
+            return;  
+      }
+
+      gridMembros.appendChild(membro);   
+}
+
+const renderizarBoxMembro = ({id, foto, nome, cargo, idade, posicao}) => {
+      const gridMemberOne = document.querySelector('.grid-member') 
+            gridMemberOne.remove()
+            
+      renderizarMembro({'id':id, 'foto':foto, 'nome':nome, 'cargo':cargo, 'idade':idade}, 'prepend');    
+}
+
+export {renderizarMembro, renderizarBoxMembro};
